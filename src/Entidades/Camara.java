04ed49ef27bb96c6b4cc91670ea3625ec7896112
MicaFurno <miqui.furno@gmail.com>
2021-02-13 20:00:48 -0300

    modificada Camara

--- src/Entidades/Camara.java
+++ src/Entidades/Camara.java
@@ -1,11 +1,33 @@
 package Entidades;
 
 public class Camara {
-	private String marca;
-	private String nombre;
-	private String sensibilidad;
-	private String formato;
+	private String marca = "";
+	private String modelo = "";
+	private Boolean soporte = false;
 
-
+	public Camara(String marca, String modelo, Boolean soporte) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.soporte = soporte;
+}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Boolean getSoporte() {
+		return soporte;
+	}
+	public void setSoporte(Boolean soporte) {
+		this.soporte = soporte;
+	}
 }
 
