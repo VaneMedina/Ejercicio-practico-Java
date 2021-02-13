3cc7e1c95dd3c1eae028c4e2b43459b6042f239a
MicaFurno <miqui.furno@gmail.com>
2021-02-13 20:01:16 -0300

    modificado el main

--- src/Tarea/Main.java
+++ src/Tarea/Main.java
@@ -1,8 +1,22 @@
 package Tarea;
 
+import Entidades.Camara;
+import Entidades.Cliente;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("holiii");	}
-
+		Camara cam = new Camara ("Canon", "Eos Rebel T7", true);	
+		System.out.println(cam.getMarca());
+		System.out.println(cam.getModelo());
+		System.out.println(cam.getSoporte());
+		
+		Cliente cliente = new Cliente("Micaela","Alsina", 1213, "sadas", 1423);
+		System.out.println(cliente.getNombre());
+		System.out.println(cliente.getDireccion());
+		System.out.println(cliente.getHistorialAlquiler());
+		System.out.println(cliente.getMesesPenalizacion());
+		System.out.println(cliente.getTelefono());
+	}
+	
 }
